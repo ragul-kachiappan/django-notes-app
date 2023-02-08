259ff1ff3b5e81f325e3518cdc982a3868a7fa02
ragul-kachiappan <rahul.kachiappan@gmail.com>
2023-02-09 03:38:15 +0530

    feat/adding create delete Modals

--- elegant_notes/notes_app/urls.py
+++ elegant_notes/notes_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = "notes_app"
 urlpatterns = [
     path('', views.home_view, name = 'home'),
+    path('delete/<int:pk>', views.delete_view, name = 'delete'),
+    path('create/', views.create_view, name = 'create'),
+
 ]
--- elegant_notes/notes_app/views.py
+++ elegant_notes/notes_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import JsonResponse
 # Create your views here.
 from .models import Note
 from .forms import NoteCreateForm,NoteUpdateForm
@@ -11,3 +12,28 @@ def home_view(request):
     notes = Note.objects.filter(author=user).order_by('updated_on')
     context = {'notes': notes}
     return render(request, 'home.html', context)
+
+def delete_view(request,pk):
+    if request.method != 'DELETE':
+        return JsonResponse({'error': 'Invalid request method. Use DELETE.'}, status=400)
+
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return JsonResponse({'error': 'Note not found'}, status=404)
+
+    note.delete()
+    return JsonResponse({'message': 'Note deleted successfully'}, status=200)
+
+def create_view(request):
+    form=NoteCreateForm()
+
+    if request.method == "POST":
+        form=NoteCreateForm(request.POST)
+
+        if form.is_valid():
+            note_obj=form.save(commit=False)
+            note_obj.author=request.user
+            note_obj.save()
+            return JsonResponse({'message': 'Note created successfully'}, status=200)
+    return JsonResponse({'message': 'Request method not allowed'}, status=405)
