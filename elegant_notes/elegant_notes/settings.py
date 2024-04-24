0d5c132f2a4f6c32df0f382a9288ba4ae3b7e7fa
ragul-kachiappan <rahul.kachiappan@gmail.com>
2024-04-24 13:47:59 +0530

    fix: correct db path to be posix path object

--- elegant_notes/elegant_notes/settings.py
+++ elegant_notes/elegant_notes/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'elegant_notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DB_PATH = "home/azureuser/db_dir"
+DB_PATH = Path("home/azureuser/db_dir")
 
 DATABASES = {
     'default': {
@@ -86,6 +86,7 @@ DATABASES = {
         'NAME': DB_PATH / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
